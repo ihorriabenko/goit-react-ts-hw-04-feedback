c5e671193ae03c0aa33853fe988e5b92103e686d
Ihor Riabenko <100082550+iryabenkoff@users.noreply.github.com>
2023-09-01 18:46:07 +0300

    ref

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import Feedback from './components/Feedback/Feedback';
 import Statistics from './components/Statistics/Statistics';
 import { FB } from './types/feedback';
+import Section from './components/Section/Section';
+import Notification from './components/Notification/Notification';
 
 const App: React.FC = (): JSX.Element => {
   const [feedback, setFeedback] = useState({
@@ -17,6 +19,12 @@ const App: React.FC = (): JSX.Element => {
     }));
   };
 
+  const countTotalFeedback = () => {
+    return Object.values(feedback).reduce((acc, el) => {
+      return acc + el;
+    }, 0);
+  };
+
   const countPositiveFeedback = (): number => {
     const { good, neutral, bad } = feedback;
     const total = good + neutral + bad;
@@ -24,13 +32,22 @@ const App: React.FC = (): JSX.Element => {
     return (good / total) * 100 > 0 ? Math.round((good / total) * 100) : 0;
   };
 
+  const total = countTotalFeedback();
+
   return (
     <div className='app'>
       <Feedback feedback={feedback} feedbackCounter={feedbackCounter} />
-      <Statistics
-        feedback={feedback}
-        countPositiveFeedback={countPositiveFeedback}
-      />
+      <Section type='h2' title='Statistics'>
+        {!total ? (
+          <Notification text={'There is no feedback'} />
+        ) : (
+          <Statistics
+            feedback={feedback}
+            total={countTotalFeedback()}
+            positiveFeedback={countPositiveFeedback()}
+          />
+        )}
+      </Section>
     </div>
   );
 };
--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -1,44 +1,34 @@
 import { FB } from '../../types/feedback';
-import Section from '../Section/Section';
-import Notification from '../Notification/Notification';
 
 interface StatisticsProps {
   feedback: FB;
-  countPositiveFeedback: () => number;
+  total: number;
+  positiveFeedback: number;
 }
 
 const Statistics: React.FC<StatisticsProps> = ({
   feedback: { good, neutral, bad },
-  countPositiveFeedback,
+  total,
+  positiveFeedback,
 }): JSX.Element => {
-  const total = good + neutral + bad;
-
   return (
-    <Section type='h2' title='Statistics'>
-      {total === 0 ? (
-        <ul className='list'>
-        <Notification text={'There is no feedback'} />
-        </ul>
-      ) : (
-        <ul className='list'>
-          <li>
-            <p>Good: {good}</p>
-          </li>
-          <li>
-            <p>Neutral: {neutral}</p>
-          </li>
-          <li>
-            <p>Bad: {bad}</p>
-          </li>
-          <li>
-            <p>Total: {good + neutral + bad}</p>
-          </li>
-          <li>
-            <p>Positive feedback: {countPositiveFeedback()} %</p>
-          </li>
-        </ul>
-      )}
-    </Section>
+    <ul className='list'>
+      <li>
+        <p>Good: {good}</p>
+      </li>
+      <li>
+        <p>Neutral: {neutral}</p>
+      </li>
+      <li>
+        <p>Bad: {bad}</p>
+      </li>
+      <li>
+        <p>Total: {total}</p>
+      </li>
+      <li>
+        <p>Positive feedback: {positiveFeedback} %</p>
+      </li>
+    </ul>
   );
 };
 
